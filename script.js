7c969fcf3cdae61fc9d7c7adb2ad95aa0367df1c
christianmonzon1 <144826568+christianmonzon1@users.noreply.github.com>
2023-12-15 12:42:34 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,36 @@
+var yourName="Christian";
+var friendsName="Diego";
+var counter=1;
+$(".sent").show();
+$(".received").css("background-color","lightgreen");
+$(".send2").click(function(){
+    var now=$(".your-input").val();
+    $(".sent").append("<p>"+now+"</p>");
+  
+});
+$(".send1").click(function(){
+    var now=$(".friend-input").val();
+    $(".received").append("<p>"+now+"</p>");
+    counter+=1;
+    $("h4").text("Number of messages received: " + counter);
+});
+
+
+
+
+
+//DON'T CHANGE THIS CODE
+
+var realYourName, realFriendName;
+
+try {
+    realYourName = eval('yourName');
+    realFriendName = eval('friendsName');
+} catch (_e) {}
+
+updateNames();
+
+function updateNames(){
+    $(".your-name").text(realYourName);
+    $(".friends-name").text(realFriendName);
+}
